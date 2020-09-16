099bda766168d45983b96666c9470c202ebed464
wmt36 <wmt36@Williams-iMac.local>
2020-09-16 09:21:42 -0600

    fixed code for final deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,10 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "employee",
-  database: "burgers_db"
-});
+const connection = mysql.createConnection(
+  process.env.JAWSDB_URL
+  
+);
 
 connection.connect(function(err) {
   if (err) {
